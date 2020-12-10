9c884e6d95a646c51bd3157b74612247060bec3d
Abhishek Bhardwaj <40014349+abhiib02@users.noreply.github.com>
2020-12-10 14:08:22 +0530

    index.js new version

--- src/index.js
+++ src/index.js
@@ -1,89 +1,137 @@
+// bake your code here
 const carouselContainer = document.querySelector(".carousel-container");
 const carouselImages = document.querySelectorAll(".carousel-slide img");
-
+const dotsContainer = document.querySelector(".dotsContainer");
 //Buttons
 const prevBtn = document.querySelector("#prevBtn");
 const nextBtn = document.querySelector("#nextBtn");
-console.log(carouselImages);
-//Counter
 
-function carousel(auto) {
-                                                        //-------------- Configurations ---------------//
-  let count = 0;                                       // Dont Change this Otherwise Some images wont show up
-  let animation_duration = 1000;                      // Set this according to you css animation duration
-  let Imagestay_duration = 1000;                     //
-  let animationClass_enter = "animate__fadeInLeft";       // Put animation css class name here
-  let animationClass2_enter = "animate__fadeInRight"
-   let animationClass_leave = "animate__fadeOutLeft";       // Put animation css class name here
-  let animationClass2_leave = "animate__fadeOutRight"
-  let autoplay = auto;                             //Boolean
-                                                  //-------------- Configurations ---------------//
-  let Image = document.createElement("img");     //----------------Creating Element of image inside Container -----------//
-  Image.src = carouselImages[count].currentSrc; //--------------- Adding Image src from the html images--------------//
-  carouselContainer.appendChild(Image);        //---------------- Inserting Image element inside Container-----------//
-  Image.classList.add("animate__animated");   //---------------- Adding Animate.css Neccessary class-----------//
-                                             //----------------- Previous Button event -----------------------//
-  prevBtn.addEventListener("click", () => {
-    if (count < 1) {
-      count = carouselImages.length - 1;
-    } else {
-      count--;
-    }
-    Image.classList.add(animationClass_leave);
-    
-    setTimeout(()=>{
-      Image.classList.remove(animationClass_leave);
-    },animation_duration)
-    setTimeout(()=>{
-      Image.src = carouselImages[count].currentSrc;  
-    },animation_duration)
+function carousel({auto,duration,interval,enterClass,leaveClass}) {
+	//-------------- Configurations ---------------//
+	//Counter 
+	let count = 0; // Dont Change this Otherwise Some images wont show up
+	let animation_duration = duration; // Set this according to you css animation duration
+	let Imagestay_duration = interval; //
+	let animation_enter = enterClass; // Put animation css class name here
+	let animation_leave = leaveClass;
+	let autoplay = auto; //Boolean
+	//-------------- Configurations ---------------//
+	let Image = document.createElement("img"); //----------------Creating Element of image inside Container -----------//
+	Image.src = carouselImages[count].currentSrc; //--------------- Adding Image src from the html images--------------//
+	carouselContainer.appendChild(Image); //---------------- Inserting Image element inside Container-----------//
+	Image.classList.add("brdr-rad-10", "animate__animated"); //---------------- Adding Animate.css Neccessary class-----------//
+	
   
-    Image.classList.add(animationClass_enter);
-    setTimeout(() => {
-      Image.classList.remove(animationClass_enter);
-    }, animation_duration*2);
-  });
-  //----------------- Previous Button event -----------------------//
-  //----------------- Next Button event -----------------------//
-  nextBtn.addEventListener("click", () => {
-    if (count > carouselImages.length - 2) {
-      count = 0;
-    } else {
-      count++;
+	for(let i = 0; i < carouselImages.length; i++) {
+		dotsContainer.innerHTML += "<a class='dot'></a>";
+	}
+	const dots = document.querySelectorAll(".dot");
+	dots.forEach((dot, index) => {
+    //----------------- Dots click event -----------------------//
+    if(!autoplay){
+      dot.addEventListener('click', () => {
+			dots[count].classList.remove('active');
+			count = index;
+			Image.classList.add(animation_leave);
+			dots[count].classList.add('active');
+			setTimeout(() => {
+				Image.classList.remove(animation_leave);
+			}, animation_duration);
+			setTimeout(() => {
+				Image.src = carouselImages[count].currentSrc;
+			}, animation_duration);
+			Image.classList.add(animation_enter);
+			setTimeout(() => {
+				Image.classList.remove(animation_enter);
+			}, animation_duration * 2);
+		})
     }
-    Image.classList.add(animationClass2_leave);
     
-    setTimeout(()=>{
-      Image.classList.remove(animationClass2_leave);
-    },animation_duration)
-    setTimeout(()=>{
-      Image.src = carouselImages[count].currentSrc;  
-    },animation_duration)
-    
-    Image.classList.add(animationClass2_enter);
-    setTimeout(() => {
-      Image.classList.remove(animationClass2_enter);
-    }, animation_duration*2);
-  });
-  //----------------- Next Button event -----------------------//
-  //----------------- Autoplay event -----------------------//
-  if (autoplay) {
-    count = 0;
-    setInterval(() => {
-      Image.classList.add(animationClass);
-      
-      Image.src = carouselImages[count].currentSrc;
-      count++;
-      console.log(Image.src);
-      if (count > carouselImages.length - 2) {
-        count = 0;
-      }
-      setTimeout(() => {
-        Image.classList.remove(animationClass);
-      }, animation_duration);
-    }, Imagestay_duration + animation_duration);
-  }
-  //----------------- Autoplay event -----------------------//
+		
+    //----------------- Dots click event -----------------------//
+	});
+	dots[count].classList.add('active');
+  //----------------- Previous Button event -----------------------//
+	prevBtn.addEventListener("click", () => {
+		dots[count].classList.remove('active');
+		if(count < 1) {
+			count = carouselImages.length - 1;
+			dots[count].classList.add('active');
+		} else {
+			count--;
+			dots[count].classList.add('active');
+		}
+		Image.classList.add(animation_leave);
+		setTimeout(() => {
+			Image.classList.remove(animation_leave);
+		}, animation_duration);
+		setTimeout(() => {
+			Image.src = carouselImages[count].currentSrc;
+		}, animation_duration);
+		Image.classList.add(animation_enter);
+		setTimeout(() => {
+			Image.classList.remove(animation_enter);
+		}, animation_duration * 2);
+	});
+	//----------------- Previous Button event -----------------------//
+	//----------------- Next Button event -----------------------//
+	nextBtn.addEventListener("click", () => {
+		dots[count].classList.remove('active');
+		if(count > carouselImages.length - 2) {
+			count = 0;
+			dots[count].classList.add('active');
+		} else {
+			count++;
+			dots[count].classList.add('active');
+		}
+		Image.classList.add(animation_leave);
+		setTimeout(() => {
+			Image.classList.remove(animation_leave);
+		}, animation_duration);
+		setTimeout(() => {
+			Image.src = carouselImages[count].currentSrc;
+		}, animation_duration);
+		Image.classList.add(animation_enter);
+		setTimeout(() => {
+			Image.classList.remove(animation_enter );
+		}, animation_duration * 2);
+	});
+	//----------------- Next Button event -----------------------//
+	//----------------- Autoplay event -----------------------//
+	if(autoplay) {
+		count = 0;
+		dots[count].classList.add('active');
+		prevBtn.remove();
+		nextBtn.remove();
+		setInterval(() => {
+			dots[count].classList.remove('active');
+			if(count > carouselImages.length - 2) {
+				count = 0;
+				dots[count].classList.add('active');
+			} else {
+				count++;
+				dots[count].classList.add('active');
+			}
+			Image.classList.add(animation_leave);
+			setTimeout(() => {
+				Image.classList.remove(animation_leave);
+			}, animation_duration);
+			setTimeout(() => {
+				Image.src = carouselImages[count].currentSrc;
+			}, animation_duration);
+			Image.classList.add(animation_enter);
+			setTimeout(() => {
+				Image.classList.remove(animation_enter);
+			}, animation_duration * 2);
+		}, Imagestay_duration + animation_duration);
+	}
+	//----------------- Autoplay event -----------------------//
 }
 
-carousel();
+carousel({
+  'auto':true,
+  'interval':4000,
+  'duration':1000,
+  'enterClass':'animate__zoomInDown',
+  'leaveClass':'animate__zoomOutDown',
+});
